a74040dd166c1a0fdd04858c3643a54621d01947
Zaid Akhter <zaidakhter1202@gmail.com>
2023-10-19 14:28:36 +0500

    disable image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,9 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   adapter: vercel({
     functionPerRoute: false,
-    imageService: true,
-    devImageService: "sharp",
-    sizes: [320, 640, 750, 828, 1080, 1200],
-    minimumCacheTTL: 60,
+    // imageService: true,
+    // devImageService: "sharp",
+    // sizes: [320, 640, 750, 828, 1080, 1200],
+    // minimumCacheTTL: 60,
   }),
 });
