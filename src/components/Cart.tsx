a5558159e4e7bda886d2bfe4d2d16fca21d617ce
shaukat456 <shaukatfullstack134@gmail.com>
2023-11-08 21:42:11 +0500

    wip

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,24 +1,45 @@
 import Layout from "@/layouts/Layout.astro";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DemoPic from "@/assets/images/craft_4.png";
 import Button from "./Button";
 import PlusIcon from "./icons/PlusIcon";
 import MinusIcon from "./icons/MinusIcon";
 import ArrowRightIcon from "./icons/ArrowRightIcon";
 import craftDetail4 from "@/assets/images/craft_detail_4.png";
+import Dropdown from "@/components/Dropdown";
+
 type CartType = {
   itemNumber?: number;
   image?: string;
   quantity?: string;
 };
 
-export const Cart = (props: CartType) => {
-  const { itemNumber = 1, image, quantity = 2 } = props;
+export const CartComponent = (props: CartType) => {
+  const { itemNumber = 1, image } = props;
 
-  const [disable, setDisable] = React.useState();
+  const [disable, setDisable] = useState<Boolean | undefined>();
+  const [quantity, setQuantity] = useState<number>(0);
+
+  const decreaseQuantity = () => {
+    if (quantity <= 0) {
+      setDisable(true);
+      return;
+    }
+    setQuantity((prevQuantity) => prevQuantity - 1);
+    setDisable(false);
+  };
+
+  const increaseQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+    setDisable(false); 
+  };
+
+useEffect(()=>{
+
+},[])
 
   return (
-    <div className="bg-gray-100 px-5 py-24 ">
+    <div className="bg-gray-100 px-3 py-12 sm:px-5 sm:py-24 ">
       <div className="w-full">
         <div className="flex flex-col">
           <h1 className="mx-0 text-5xl font-extralight sm:mx-5 sm:text-7xl">
@@ -28,80 +49,89 @@ export const Cart = (props: CartType) => {
             <div className="inline-block min-w-full py-2 align-middle">
               <div className="overflow-hidden border-b border-gray-400  ">
                 <table className="min-w-full divide-y divide-gray-400">
-                  <thead className="items-start">
+                  <thead className="">
                     <tr>
                       <th
                         scope="col"
-                        className="w-2/6 py-3 text-left text-xs font-semibold tracking-wider text-black sm:text-sm"
+                        className="w-5/12 py-3 text-left text-xs font-bold tracking-wider text-black sm:text-sm"
                       >
                         Products
                       </th>
                       <th
                         scope="col"
-                        className="w-1/6 px-4 py-3 text-left text-xs font-medium tracking-wider sm:px-6 sm:text-sm"
+                        className="w-2/12 px-4 py-3 text-left text-xs   font-bold tracking-wider sm:px-3 sm:text-sm"
                       >
                         Size
                       </th>
                       <th
                         scope="col"
-                        className="w-3/6 px-4 py-3 text-left text-xs font-medium tracking-wider sm:px-6 sm:text-sm"
+                        className="w-4/12 px-4 py-3 text-left text-xs font-bold  tracking-wider sm:px-3 sm:text-sm"
                       >
                         Quantity
                       </th>
                       <th
                         scope="col"
-                        className="w-1/6 px-4 py-3 text-left text-xs font-medium tracking-wider sm:px-6 sm:text-sm"
+                        className="w-1/12 px-4 py-3 text-left text-xs font-bold  tracking-wider sm:px-3 sm:text-sm"
                       >
                         Price
                       </th>
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 ">
-                    <tr> 
+                    <tr>
                       <td className="py-4 sm:py-6 ">
-                        <div className="sm:flex  items-center sm:gap-2 ">
+                        <div className="items-center sm:flex sm:gap-2 ">
                           <img
                             src={craftDetail4.src}
                             alt="demoImage"
-                            className="sm:h-32 h-20 object-bottom md:w-2/6 sm:w-3/6  w-4/6  "
+                            className="h-20 w-4/6 object-fill sm:h-32 sm:w-3/6  md:w-1/6"
                           />
-                          <div className="flex flex-col sm:space-y-14 sm:text-start text-center">
-                            <h1 className="text-lg sm:text-2xl">watch</h1>
-                            <button className="underline sm:text-sm">
+                          <div className="flex flex-col text-center sm:space-y-14 sm:text-start">
+                            <h1 className="text-lg sm:text-2xl ">watch</h1>
+                            <button className="text-xs underline sm:text-sm ">
                               Remove
                             </button>
                           </div>
                         </div>
                       </td>
-                      <td className="px-4 py-4 sm:px-6 sm:py-6 items-start">Dropdown</td>
-                      <td className="px-4 py-4 sm:px-6 sm:py-6">
-                        <div className="flex items-center gap-2 sm:gap-5">
-                          <button className="border border-gray-500 p-2 text-black sm:p-3">
+                      <td className="flex p-2 sm:p-3">
+                        <Dropdown />
+                      </td>
+                      <td className="p-2 sm:p-3 ">
+                        <div className="flex  items-center  gap-2 sm:gap-5   ">
+                          <button
+                            className="border border-gray-500 p-2 text-black sm:p-3"
+                            onClick={() => decreaseQuantity()}
+                            disabled={!!disable}
+                          >
                             <MinusIcon className="h-2 w-2 text-black sm:h-3 sm:w-3" />
                           </button>
-                          {quantity}
+                          <h1 className={disable ?  "text-gray-300" : "text-black"}>
+
+                            {quantity}
+                            </h1>
                           <button
                             className="border border-gray-500 p-2 sm:p-3"
                             aria-label="View details"
+                            onClick={() => increaseQuantity()}
                           >
                             <PlusIcon className="h-2 w-2 text-black sm:h-3 sm:w-3" />
                           </button>
                         </div>
                       </td>
-                      <td className="px-4 py-4 sm:px-6 sm:py-6">$625.5</td>
+                      <td className="flex p-2 sm:p-3">$625.5</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-
             </div>
-              <div className="my-6 flex flex-col justify-between gap-5 sm:my-10  sm:flex-row sm:gap-0 items-center">
-                <h1>Shipping will be calculated during checkout</h1>
-                <Button className="gap-6 bg-black font-extralight text-white hover:bg-black">
-                  Check Out
-                  <ArrowRightIcon className="h-6 w-6 stroke-white stroke-1 sm:h-7 sm:w-8" />
-                </Button>
-              </div>
+          </div>
+          <div className="my-6 flex flex-col items-center justify-between gap-5  px-1 sm:my-10 sm:flex-row sm:gap-0  sm:px-7">
+            <h1>Shipping will be calculated during checkout</h1>
+            <Button className="gap-6 bg-black font-extralight text-white hover:bg-black">
+              Check Out
+              <ArrowRightIcon className="h-6 w-6 stroke-white stroke-1 sm:h-7 sm:w-8" />
+            </Button>
           </div>
         </div>
       </div>
