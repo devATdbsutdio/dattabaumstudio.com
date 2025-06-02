11b574dba21288c90d90f2decf9f8909ad6a7024
Utkarsh Namdeo <utkarshnamdeo@live.com>
2025-06-02 10:09:21 +0200

    chore: upgrade to astro v5, major version upgrade with breaking changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   integrations: [react(), tailwind()],
+  security: {
+    checkOrigin: false
+  },
   adapter: vercel({
-    functionPerRoute: false,
     imageService: false,
     // devImageService: "sharp",
     // sizes: [320, 640, 750, 828, 1080, 1200],
